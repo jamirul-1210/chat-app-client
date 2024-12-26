40c5ffc20f609448d508c4025123b7aeeabe58f0
Jamirul Islam <myself.jamirul@gmail.com>
2024-12-26 11:30:47 +0530

    backend url updated

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+export const BACKEND_BASE_URL = 'https://chat-api.jamirul.site';
 export const AUTH_TOKEN_KEY_NAME = 'auth_token';
 export const AUTH_USER_KEY_NAME = 'auth_user';
